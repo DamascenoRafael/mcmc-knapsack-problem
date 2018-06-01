92b511d26af816612719859f80cb30b774450af7
Rafael Damasceno <DamascenoRafael@users.noreply.github.com>
2018-06-01 15:51:40 -0300

    Add LazyRandomWalk

--- knapsack-problem.py
+++ knapsack-problem.py
@@ -18,6 +18,7 @@ currentSolution = {
     'v': 0,
     'w': 0
 }
+
 bestSolution = {
     'items': [],
     'v': 0,
@@ -25,13 +26,14 @@ bestSolution = {
 }
 
 def readProblemFrom(name):
+    global n, maxWeight, optimum, bestSolution
     with open(name) as f:
-        n = f.readline()
-        maxWeight = map(int, f.readline().split())[1]
-        optimum = map(int, f.readline().split())[1]
+        n = int(f.readline().split()[1])
+        maxWeight = int(f.readline().split()[1])
+        optimum = int(f.readline().split()[1])
         f.readline() # time
         for line in f:
-            _, lineV, lineW, _ = map(int, line.split(','))
+            _, lineV, lineW, _ = list(map(int, line.split(',')))
             v.append(lineV)
             w.append(lineW)
             if random.random() < 0.5 and isValidStateWith(lineW):
@@ -43,44 +45,52 @@ def readProblemFrom(name):
         bestSolution = currentSolution.copy()
 
 def isValidStateWith(w):
-    return currentSolution['w'] + lineW <= maxWeight
+    return currentSolution['w'] + w <= maxWeight
 
 def addItem(item, solution):
     solution['items'][item] = 1
     solution['w'] += w[item]
     solution['v'] += v[item]
+    return solution.copy()
 
 def removeItem(item, solution):
     solution['items'][item] = 0
     solution['w'] -= w[item]
     solution['v'] -= v[item]
+    return solution.copy()
+
+def isBetterSolution(solution):
+    return (solution['v'] > bestSolution['v']) or (solution['v'] == bestSolution['v'] and solution['w'] < bestSolution['w'])
 
 def newStates():
+    global currentSolution
     possibleStates = []
-    # gerar aleatorio
-    # ver qual o vértice
-    # ver se ele já está na sol
-    # se tiver retiro
-    # se não tiver vejo se pode ser adicionado
     for i in range(n):
         solution_i = currentSolution.copy()
         if solution_i['items'][i] == 0:
             if isValidStateWith(w[i]):
-                addItem(i, solution_i)
+                solution_i = addItem(i, solution_i)
                 possibleStates.append(solution_i.copy())
         else:
-            removeItem(i, solution_i)
+            solution_i = removeItem(i, solution_i)
             possibleStates.append(solution_i.copy())
     
     return possibleStates
 
+def lazyRandomWalk():
+    global bestSolution, currentSolution
+    count = 0
+    while(count<10):
+        count += 1
+        unif = random.random()
+        if unif < 0.5:
+            possibleStates = newStates()
+            index = random.randint(0, len(possibleStates)-1)
+            newState = possibleStates[index]
+            currentSolution = newState.copy()
+            if isBetterSolution(currentSolution):
+                bestSolution = currentSolution.copy()
 
-def currentValue():
-    pass
 
-def currentWeight():
-    pass
-
-def remainingWeight():
-    pass
-    #return maxWeight - currentWeight()
+readProblemFrom('teste.csv')
+lazyRandomWalk()
